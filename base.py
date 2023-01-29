Fixed bug in objective stand along computation

base.py
@@ -34,8 +34,19 @@ print("Tensorflow version %s running on %ld CPUs and %ld GPUs" % (tf.__version__
 dh_dtype = tf.float32
 tf.keras.backend.set_floatx(dh_dtype.name)
 
-DIM_DUMMY = "_dimension_dummy"   # added to each world data dictionary to allow code to always asses the number of samples. Objkect is to be of dimension [None,]
+"""
+DIM_DUMMY
+Each world data dictionary must have an element with this name, whcih needs to be of dimension (None,1)
+This is used in layers.VariableLayer in order to scale the variable up to the number of samples
 
+The reason the dimension is (None,1) and not (None,) is that older Tensorflow versions auto-upscale data
+of dimension (None,) to (None,1) anyway.
+
+It's not a pretty construction but there seems to be no other nice way in TF to find our current sample size.
+"""                                
+
+DIM_DUMMY = "_dimension_dummy"  
+        
 # -------------------------------------------------
 # TF <--> NP
 # -------------------------------------------------

layers.py
@@ -44,20 +44,23 @@ class VariableLayer(tf.keras.layers.Layer):
         """
         self._available_features = sorted( [ str(k) for k in shapes if not k == DIM_DUMMY ] )
         dummy_shape = shapes.get(DIM_DUMMY, None)
-        _log.verify( not dummy_shape is None, "Every data set must have a member '%s' (value of base.DIM_DUMMY) of dimension [None,]. Found data: %s", DIM_DUMMY, self.available_features )
-        _log.verify( len(dummy_shape) == 2 and dummy_shape[1] == 1, "Every data set must have a member '%s' (value of base.DIM_DUMMY) of dimension [None,1]. Found data: %s of shape %s", DIM_DUMMY, self.available_features, dummy_shape.as_list() )
+        _log.verify( not dummy_shape is None, "Every data set must have a member '%s' (see base.DIM_DUMMY) of shape (None,1). Data member not found data: %s", DIM_DUMMY, list(self.available_features) )
+        _log.verify( len(dummy_shape) == 2, "Data set member '%s' (see base.DIM_DUMMY) nust be of shape [None,1], not of shape %s", DIM_DUMMY, dummy_shape.as_list() )
+        _log.verify( int(dummy_shape[1]) == 1, "Data set member '%s' (see base.DIM_DUMMY) nust be of shape [None,1], not of shape %s", DIM_DUMMY, dummy_shape.as_list() )
         
     def call( self, dummy_data : dict = None, training : bool = False ) -> tf.Tensor:
         """
         Return variable value
-        The returned tensor will be of dimension [None,] if self is a float, and otherwise of dimension [None, ...]
-        
-        The 'dummy_data' dictionary must have an element DIM_DUMMY of dimension [None,1]
+        The returned tensor will be of dimension [None,] if self.variable is a float, and otherwise of dimension [None, ...] where '...' refers to the dimension of the variable.        
+
+        The 'dummy_data' dictionary must have an element DIM_DUMMY of dimension (None,).
         """
-        dummy = dummy_data[DIM_DUMMY][:,0]
-        if len(self.variable.shape) == 0:
-            return dummy*0. + self.variable
-        x = (dummy*0.)[:,tf.newaxis] + self.variable[tf.newaxis,...]
+        dummy = dummy_data[DIM_DUMMY]
+        assert len(dummy.shape) == 2, "Internal error: shape %s not (None,)" % str(dummy.shape.as_list())
+        x     = tf.zeros_like(dummy[:,0])
+        while len(x.shape) <= len(self.variable.shape):
+            x = x[:,tf.newaxis,...]
+        x = x + self.variable[tf.newaxis,...]
         return x
     
     @property

objectives.py
@@ -7,7 +7,7 @@ June 30, 2022
 @author: hansbuehler
 """
 
-from .base import Logger, Config, tf, dh_dtype, tfCast, fmt_list
+from .base import Logger, Config, tf, dh_dtype, tfCast, fmt_list, DIM_DUMMY
 from .layers import DenseLayer, VariableLayer
 from cdxbasics import PrettyDict as pdct
 from collections.abc import Mapping
@@ -171,6 +171,7 @@ class MonetaryUtility(tf.keras.Model):
         """
         Computes the utility of a payoff with classic optimization.
         This function only works if 'y' is stateless.
+        Note that this function is not very accurate. Use trainer.train_utillity() for a more accurate version using tensorflow.
         
         Parameters
         ----------
@@ -184,7 +185,7 @@ class MonetaryUtility(tf.keras.Model):
         
         try:
             # trigger build(). This will fail if 'y' expects some features 
-            _ = self.y(data={})
+            _ = self.y(data={DIM_DUMMY:(payoff*0.)[:,np.newaxis]})
         except KeyError as k:
             _log.verify( self.nFeatures == 0, "Utility intercept 'y' relies on %ld features %s. Cannot compute simple initial utility. Use TensorFlow.", self.nFeatures,self.features )
 
@@ -196,17 +197,19 @@ class MonetaryUtility(tf.keras.Model):
         sample_weights   = sample_weights[:,0] if not sample_weights is None and len(sample_weights.shape) == 2 and sample_weights.shape[1] == 1 else sample_weights
         
         _log.verify( payoff.shape == sample_weights.shape, "'payoff' must have same shape as 'sample_weights'. Found %s and %s, respectively", payoff.shape, sample_weights.shape )
-        
+
+        payoff           = tf.convert_to_tensor(payoff)
+
         def objective(y):
             y = np.array(y, dtype=np_dtype)
-            r = utility(self.utility, self.lmbda, tf.convert_to_tensor(payoff), y=tf.convert_to_tensor(y) )
+            r = utility(self.utility, self.lmbda, payoff, y=tf.convert_to_tensor(y) )
             u = np.array( r.u, dtype=np_dtype )
             u = np.sum( sample_weights * u )
             return -u
 
-        r0 = -objective(0.)  # will throw any errors
+        _  = objective(0.)   # triggers errors if any
         r  = minimize_scalar( objective, **minimize_scalar_kwargs )
-        _log.verify( r.success, "Failed to find optimal intercept 'y' for utility %s with risk aversion %g: %s", self.utility, self.lmbda, r.message )
+        if not r.success: _log.throw( "Failed to find optimal intercept 'y' for utility %s with risk aversion %g: %s", self.utility, self.lmbda, r.message )
         return -r.x
         
         

world.py
@@ -386,7 +386,7 @@ class SimpleWorld_Spot_ATM(object):
             
         # the following variables must always be present in any world
         # it allows to cast dimensionless variables to the number of samples
-        self.data.features.per_path[DIM_DUMMY] = payoff * 0.
+        self.data.features.per_path[DIM_DUMMY] = (payoff * 0.)[:,np.newaxis]  # (None,1)
 
         # check numerics
         assert_iter_not_is_nan( self.data, "data" )